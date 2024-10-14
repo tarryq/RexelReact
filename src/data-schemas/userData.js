8a1946508dd0d90f5a334e56f1a848d7ecc864d8
KASMI7 <sunilkumarkasmi@gmail.com>
2024-10-14 14:39:43 +0530

    add data

---
+++ src/data-schemas/userData.js
@@ -0,0 +1,141 @@
+export const users = [
+  // Apple Store Users
+  {
+    username: 'storeuser1',
+    password: '121212',
+    usertype: 'store',
+    name: 'Paul',
+    accountname: 'Apple',
+    storename: 'Apple Store 1',
+    menu: ['Home', 'Maintenance'],
+    email: 'storeuser1@apple.com'
+  },
+  {
+    username: 'storeuser2',
+    password: '121212',
+    usertype: 'store',
+    name: 'Anna',
+    accountname: 'Apple',
+    storename: 'Apple Store 2',
+    menu: ['Home', 'Maintenance'],
+    email: 'storeuser2@apple.com'
+  },
+  {
+    username: 'accountuser_apple',
+    password: '121212',
+    usertype: 'account',
+    name: 'Steve',
+    accountname: 'Apple',
+    storename: null,
+    menu: ['Home', 'Maintenance'],
+    email: 'account@apple.com'
+  },
+
+  // Samsung Store Users
+  {
+    username: 'storeuser3',
+    password: '121212',
+    usertype: 'store',
+    name: 'Chris',
+    accountname: 'Samsung',
+    storename: 'Samsung Store 1',
+    menu: ['Home', 'Support'],
+    email: 'storeuser1@samsung.com'
+  },
+  {
+    username: 'storeuser4',
+    password: '121212',
+    usertype: 'store',
+    name: 'Jenny',
+    accountname: 'Samsung',
+    storename: 'Samsung Store 2',
+    menu: ['Home', 'Support'],
+    email: 'storeuser2@samsung.com'
+  },
+  {
+    username: 'accountuser_samsung',
+    password: '121212',
+    usertype: 'account',
+    name: 'David',
+    accountname: 'Samsung',
+    storename: null,
+    menu: ['Home', 'Support'],
+    email: 'account@samsung.com'
+  },
+
+  // Adidas Store Users
+  {
+    username: 'storeuser5',
+    password: '121212',
+    usertype: 'store',
+    name: 'Ethan',
+    accountname: 'Adidas',
+    storename: 'ADIDAS AMERICA INC - PORTLAN',
+    menu: ['Home', 'Orders'],
+    email: 'storeuser1@adidas.com'
+  },
+  {
+    username: 'storeuser6',
+    password: '121212',
+    usertype: 'store',
+    name: 'Mia',
+    accountname: 'Adidas',
+    storename: 'ADIDAS EUROPE INC - BERLIN',
+    menu: ['Home', 'Orders'],
+    email: 'storeuser2@adidas.com'
+  },
+  {
+    username: 'accountuser_adidas',
+    password: '121212',
+    usertype: 'account',
+    name: 'Liam',
+    accountname: 'Adidas',
+    storename: null,
+    menu: ['Home', 'Orders'],
+    email: 'account@adidas.com'
+  },
+
+  // Nike Store Users
+  {
+    username: 'storeuser7',
+    password: '121212',
+    usertype: 'store',
+    name: 'Sophie',
+    accountname: 'Nike',
+    storename: 'Nike Store NYC',
+    menu: ['Home', 'Sales'],
+    email: 'storeuser1@nike.com'
+  },
+  {
+    username: 'storeuser8',
+    password: '121212',
+    usertype: 'store',
+    name: 'Jake',
+    accountname: 'Nike',
+    storename: 'Nike Store LA',
+    menu: ['Home', 'Sales'],
+    email: 'storeuser2@nike.com'
+  },
+  {
+    username: 'accountuser_nike',
+    password: '121212',
+    usertype: 'account',
+    name: 'Mark',
+    accountname: 'Nike',
+    storename: null,
+    menu: ['Home', 'Sales'],
+    email: 'account@nike.com'
+  },
+
+  // Admin User
+  {
+    username: 'adminuser',
+    password: '121212',
+    usertype: 'admin',
+    name: 'John',
+    accountname: null,
+    storename: null,
+    menu: ['Home', 'Maintenance'],
+    email: 'admin@abc.com'
+  }
+];
