3f290a4e41a9f45d50f915cf8eeb7c9c8e4a9a32
sreenath <ysree6767@gmail.com>
2025-01-05 10:58:33 +0530

    minor changes

--- src/store/features/products/productActions.js
+++ src/store/features/products/productActions.js
@@ -1,31 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { getProductColumns, getProducts } from './productsApi';
 
-// export const fetchProductColumns = createAsyncThunk(
-//  'products/fetchProductColumns',
-//  async ({ userId, accountId }, thunkAPI) => {
-
-//   try {
-//    const response = await getProductColumns(userId, accountId);
-//    return response;
-//   } catch (error) {
-//    return thunkAPI.rejectWithValue(error.response?.data || 'Failed to fetch product columns');
-//   }
-//  }
-// );
-
-// export const fetchProducts = createAsyncThunk(
-//  'products/fetchProducts',
-//  async ({ userId, accountId, storeId }, thunkAPI) => {
-//   try {
-//    const response = await getProducts(userId, accountId, storeId);
-//    return response;
-//   } catch (error) {
-//    return thunkAPI.rejectWithValue(error.response?.data || 'Failed to fetch products');
-//   }
-//  }
-// );
-
 export const fetchProductColumns = createAsyncThunk(
   'products/fetchProductColumns',
   async ({ userid, accountid }) => {
